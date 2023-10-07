ce76371a361c3a851f9ebdf4e1d9f6be4d0f8593
Evolvifi <evolvifi@gmail.com>
2023-10-07 14:00:26 -0500

    created product cards

--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
+"use client";
+
+import { Toaster } from "react-hot-toast";
 import App from "./_app";
 import AppLayout from "./components/Layout";
+import { CartProvider } from "use-shopping-cart";
 
 export default function Home() {
-  return <App />;
+  const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+
+  return (
+    <CartProvider cartMode="checkout-session" stripe={stripeKey} currency="USD">
+      <App />
+      <Toaster />
+    </CartProvider>
+  );
 }
